a17c5aa87dc06d5c77b817c3467b049b07b29f63
胡矿 <thiswind@hukuangdeMacBook-Pro.local>
2018-12-24 13:37:21 +0800

    init

--- urlping.py
+++ urlping.py
@@ -1,21 +1,35 @@
+#/usr/bin/env python
+
 import requests
 import datetime
+import time
 
-import sys
-import getopt
+import argparse
 
-def main(argv):
-    url = argv[0]
-
-    # url = 'http://xk.ynu.edu.cn'
-
+def do_ping(url):
     start = datetime.datetime.now()
     r = requests.get(url)
     end = datetime.datetime.now()
     elapsed = end - start
 
-    print('get response from', url, '; status:', r.status_code, '; time elapsed(in seconds):', elapsed.total_seconds())
+    print('get response from:', url, '; status:', r.status_code, '; time elapsed(in seconds):', elapsed.total_seconds())
 
+def main():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--url', required=True, help='url to ping')
+    parser.add_argument('--times', required=False, help='total ping times')
+
+    args = parser.parse_args()
+
+    times = int(args.times)
+    url = args.url
+
+    for i in range(times):
+        do_ping(url)
+        time.sleep(1)
+        
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
